e2877aa1d5374d1ca30827db3e19e5f26051847c
Abhay <dhumneabhay@gmail.com>
2026-02-11 16:12:26 +0530

    updated port

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ app.use("/db", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is listnening on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => console.log("running on", PORT));
+
